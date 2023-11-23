8dcb5941a7742ef614eac3bdd26cabb4d8f4ad99
Rodrigo Alonzo <ralonzo1337@gmail.com>
2023-11-22 21:13:07 -0300

    Deleted extra folder - fixed navbar

---
+++ src/components/Burguerbutton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/Burguerbutton.css"
+
+function BurguerButton() {
+    return (
+        <div class="icon nav-icon-5">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+    )
+}
+
+export default BurguerButton;
--- src/components/Navbar copy.jsx
+++ src/components/Navbar copy.jsx
@@ -12,7 +12,7 @@ export const Nav = () => {
             <div className="navImgContainer"><img className="Navbar-img" src="/src/assets/logo2.png" alt="" />
             </div>
             <div className="link-container">
-                /*<ul className="un-list">
+                <ul className="un-list">
                     <li className="list-link"><a className="Link" href="">Menu</a>
                     </li>
                     <li className="list-link"><a className="Link" href="">Servicios</a>
