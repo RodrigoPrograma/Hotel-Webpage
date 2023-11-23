653f9b44203edae372441e6a18576f0beb2e0876
Rodrigo Alonzo <ralonzo1337@gmail.com>
2023-11-23 13:30:54 -0300

    Barra de navegacion lista

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import React from "react";
 import "../Styles/Navbar.css"
 import BurguerButton from "./Burguerbutton";
+import styled from "styled-components";
 
 
 
 function Nav () {
 
-    const [clicked, setClicked] = useState(false)
+    const [clicked, setClicked] = useState(false);
     
     const handleClick = () => {
         setClicked(!clicked)
@@ -20,27 +21,50 @@ function Nav () {
                 <div className="navbarContainer">
                     <div className="navImgContainer"><img className="Navbar-img" src="/src/assets/logo2.png" alt="" />
                     </div>
-                    <div className={"link-container ${clicked ? '' : 'active'}"}>  
-                            <li className="list-link"><a className="Link" href="">Menu</a>
+                    <div className={`link-container ${clicked ? 'active' : ''}`}>  
+                            <li onClick ={handleClick} className="list-link"><a className="Link" href="#">Menu</a>
                             </li>
-                            <li className="list-link"><a className="Link" href="">Servicios</a>
+                            <li onClick ={handleClick} className="list-link"><a className="Link" href="#">Servicios</a>
                             </li>
-                            <li className="list-link"><a className="Link" href="">Nosotros</a>
+                            <li onClick ={handleClick} className="list-link"><a className="Link" href="#">Nosotros</a>
                             </li>
-                            <li className="list-link"><a className="Link" href="">Habitaciones</a>
+                            <li onClick ={handleClick} className="list-link"><a className="Link" href="#">Habitaciones</a>
                             </li>
-                            <li className="list-link"><a className="Link" href="">Contacto</a>
+                            <li onClick ={handleClick} className="list-link"><a className="Link" href="#">Contacto</a>
                             </li>
                     </div>
                         <BurguerButton clicked = {clicked} handleClick ={handleClick}/>
                 </div>
                 <div className="paragraphContainer">
                     <p className="navParagraph">Abre la puerta para una vida espaciosa</p>
+                    
                 </div>
             </div>
+                <Bgdiv className={`initial ${clicked ? 'active' : ''}`}></Bgdiv>
         </>
     );
 
 };
 
 export default Nav
+
+const Bgdiv = styled.div`
+
+    position: absolute;
+    background-color: #121212;
+    top: -700px;
+    left: -1000px;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+    transition: all .6s ease;
+    
+    &.active{
+        border-radius: 0 0 80% 0;
+        display: inline-block;              
+           top: 0;
+           left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    `
